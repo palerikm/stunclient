97654d9008a9764ce8ed2b71848bf480d62d7836
Pål-Erik Martinsen <palerikm@gmail.com>
2017-01-11 19:03:40 +0100

    Single script.

--- serverping.sh
+++ serverping.sh
@@ -3,16 +3,59 @@
 output_file="out.csv"
 
 filename=server_list.conf
-curl -s https://osaf70665k.execute-api.us-west-2.amazonaws.com/prd/discovery-config | jq -r '.[] | .[] | [.candidateIP, .candidatePort] |@csv' > $filename
 
-rm $output_file
-printf "Start,Stop,transactionID,ServerAddr,ClientAddr,RFLXAddr,RTT,retries,clientsent,serversent\n" > $output_file
+interface="eth0"
+ip=""
+runs=10
+output_file="out.json"
+test_name="test"
+
+tmp_csv="tmp.csv"
+post_server="http://ec2-35-166-234-254.us-west-2.compute.amazonaws.com/discovery-result"
+
+while getopts "h?i:f:r:o:t:p:" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    r)  runs=$OPTARG
+        ;;
+    i)  interface=$OPTARG
+        ;;
+    f)  output_file=$OPTARG
+        ;;
+    t)  test_name=$OPTARG
+        ;;
+    p)  post_server=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+ip=$@
+
+curl -s https://osaf70665k.execute-api.us-west-2.amazonaws.com/prd/discovery-config | jq -r '.[] | .[] | [.candidateIP, .candidatePort] |@csv' > $filename
+#cat discovery-config | jq -r '.[] | .[] | [.candidateIP, .candidatePort] |@csv' > $filename
+
+rm $tmp_csv
+printf "Start,Stop,transactionID,ServerAddr,ClientAddr,RFLXAddr,RTT,retries,clientsent,serversent\n" > $tmp_csv
 while IFS=, read ip_q port
 do
   ip=$(echo "$a" | sed -e 's/^"//' -e 's/"$//' <<<"$ip_q")
-  echo "I got:$ip on port $port"
-  ./nattorture.sh -i en7 -r 2 -f tmp.csv -p $port $ip
-  cat tmp.csv >> $output_file
+  for (( i=1; i<=$runs; i++ ))
+  do
+    echo "Run $i"
+   build/dist/bin/stunclient -i $interface $ip -p $port -j 60 --csv >> $tmp_csv
+  done
+  #./nattorture.sh -i en7 -r 2 -f tmp.csv -p $port $ip
+  #cat tmp.csv >> $output_file
 done < "$filename"
 
-csv2json out.csv -o $@
+csv2json $tmp_csv $output_file
+
+echo "Posting to $post_server"
+curl -H "Content-Type: application/json" -X POST --data @$output_file $post_server
