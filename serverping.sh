b10b7f4e8560b7f3859e2afd1758323763a906bf
Pål-Erik Martinsen <palerikm@gmail.com>
2017-01-11 19:55:21 +0100

    Fixed discovery

--- serverping.sh
+++ serverping.sh
@@ -12,7 +12,7 @@ test_name="test"
 
 tmp_csv="tmp.csv"
 post_server="http://ec2-35-166-234-254.us-west-2.compute.amazonaws.com/discovery-result"
-
+server_list="http://ec2-35-166-234-254.us-west-2.compute.amazonaws.com/discovery-config.json"
 while getopts "h?i:f:r:o:t:p:" opt; do
     case "$opt" in
     h|\?)
@@ -38,7 +38,7 @@ shift $((OPTIND-1))
 
 ip=$@
 
-curl -s https://osaf70665k.execute-api.us-west-2.amazonaws.com/prd/discovery-config | jq -r '.[] | .[] | [.candidateIP, .candidatePort] |@csv' > $filename
+curl -s $server_list | jq -r '.[] | [.ip, .port] |@csv' > $filename
 #cat discovery-config | jq -r '.[] | .[] | [.candidateIP, .candidatePort] |@csv' > $filename
 
 rm $tmp_csv
@@ -46,9 +46,10 @@ printf "Start,Stop,transactionID,ServerAddr,ClientAddr,RFLXAddr,RTT,retries,clie
 while IFS=, read ip_q port
 do
   ip=$(echo "$a" | sed -e 's/^"//' -e 's/"$//' <<<"$ip_q")
+  echo "Pinging $ip at $port"
   for (( i=1; i<=$runs; i++ ))
   do
-    echo "Run $i"
+   echo "  Run $i"
    build/dist/bin/stunclient -i $interface $ip -p $port -j 60 --csv >> $tmp_csv
   done
   #./nattorture.sh -i en7 -r 2 -f tmp.csv -p $port $ip
